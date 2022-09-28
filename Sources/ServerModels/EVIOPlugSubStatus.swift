d1b7098354bd7ca4fef0ce5653d26fabe33e278f
Joao Moreira <joao.moreira@go-evio.com>
2022-09-28 14:16:27 +0100

    added initialized to the evio plug substatus enum

--- Sources/ServerModels/EVIOPlugSubStatus.swift
+++ Sources/ServerModels/EVIOPlugSubStatus.swift
@@ -19,4 +19,27 @@ public enum EVIOPlugSubSatus: String, Codable {
     case reserved = "RESERVED"
     case unknown = "UNKNOWN"
     
+    public init?(rawValue: String) {
+        switch rawValue.uppercased() {
+        case Self.available.rawValue:
+            self = .available
+        case Self.blocked.rawValue:
+            self = .blocked
+        case Self.charging.rawValue:
+            self = .charging
+        case Self.inoperative.rawValue:
+            self = .inoperative
+        case Self.outOfOrder.rawValue:
+            self = .outOfOrder
+        case Self.planned.rawValue:
+            self = .planned
+        case Self.removed.rawValue:
+            self = .removed
+        case Self.reserved.rawValue:
+            self = .reserved
+        default:
+            self = .unknown
+        }
+    }
+    
 }

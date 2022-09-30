fb90b08f25ff83145ad747f3ec60d8eb7a58f6e1
Joao Moreira <joao.moreira@go-evio.com>
2022-09-30 10:51:12 +0100

    Added new extension for String type with the names of the week days

---
+++ Sources/Extensions/String+WeekDays.swift
@@ -0,0 +1,22 @@
+//
+//  String+WeekDays.swift
+//  
+//
+//  Created by Tiago Moreira on 30/09/2022.
+//
+
+import Foundation
+
+public typealias EVIOWeekDay = String
+
+public extension String {
+    
+    static let sunday = "SUNDAY"
+    static let monday = "MONDAY"
+    static let tuesday = "TUESDAY"
+    static let wednesday = "WEDNESDAY"
+    static let thursday = "THURSDAY"
+    static let friday = "FRIDAY"
+    static let saturday = "SATURDAY"
+    
+}

0cef0b254b8dd7ec0dcc4f8c5af92875cc2b8cb8
Joao Moreira <joao.moreira@go-evio.com>
2022-10-17 12:32:52 +0100

    Fixed a duplicated line

--- Sources/Extensions/String+Endpoints.swift
+++ Sources/Extensions/String+Endpoints.swift
@@ -125,6 +125,5 @@ public extension String {
     static let updateContractPaymentMethod: EVIOEndpoints = "/private/contracts/paymentMethod"
     static let getOcpiDetailedTariffsEndpoint: EVIOEndpoints = "/public/ocpi/detailedTariffs"
     static let cancelCardEndpoint: EVIOEndpoints = "/private/contracts/cancelRFID"
-    static let getPlafondMovementsEndpoint: EVIOEndpoints = "/private/history_v2/byEV"
     
 }

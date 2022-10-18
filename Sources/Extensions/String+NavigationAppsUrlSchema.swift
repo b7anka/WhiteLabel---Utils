e3bc26994d04e9b5b4ea94e1fd5ba50581871874
Joao Moreira <joao.moreira@go-evio.com>
2022-10-18 11:13:58 +0100

    Fixed a bug with the navigation apps schemas

--- Sources/Extensions/String+NavigationAppsUrlSchema.swift
+++ Sources/Extensions/String+NavigationAppsUrlSchema.swift
@@ -10,8 +10,8 @@ import Foundation
 public extension String {
     
     static let googleSchema: String = "comgooglemaps://"
-    static let appleSchema: String = "waze://"
-    static let wazeSchema: String = "http://maps.apple.com/"
+    static let wazeSchema: String = "waze://"
+    static let appleSchema: String = "http://maps.apple.com/"
     static let tomtomSchema: String = "tomtomgo://"
     
 }

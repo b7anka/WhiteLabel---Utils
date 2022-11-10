ade4915d2866edfcd75b99e525642663544c8b04
Joao Moreira <joao.moreira@go-evio.com>
2022-11-10 16:42:29 +0000

    Still implementing new component

--- Sources/Components/EVIOCheckBoxButton.swift
+++ Sources/Components/EVIOCheckBoxButton.swift
@@ -21,21 +21,21 @@ public struct EVIOCheckBoxButton: View {
         Button(action: self.toggleSelected) {
             Rectangle()
                 .fill(Color.clear)
-                .frame(width: 20, height: 20)
+                .frame(width: 22, height: 22)
                 .background(
                     Rectangle()
                     .stroke(Color.mainButtonColor, lineWidth: 1)
-                    .frame(width: 19, height: 19)
+                    .frame(width: 20, height: 20)
                     .overlay(
                         Rectangle()
                             .fill(self.selected ? Color.mainButtonColor : Color.clear)
-                            .frame(width: 18, height: 18)
+                            .frame(width: 20, height: 20)
                             .overlay(
                                 Image(systemName: .checkmark)
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
                                     .foregroundColor(Color.white)
-                                    .frame(width: 18, height: 18)
+                                    .frame(width: 20, height: 20)
                             )
                 )
             )

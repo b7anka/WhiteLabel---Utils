8003c62c02a8249716fd2c3226e4c36b49dd7bc2
Joao Moreira <joao.moreira@go-evio.com>
2022-12-16 12:26:34 +0000

    Added new component

---
+++ Sources/Components/UIGrid.swift
@@ -0,0 +1,58 @@
+//
+//
+//
+//
+//
+//
+//
+
+import SwiftUI
+
+struct UIGrid<Content: View, T: Hashable>: View {
+    
+    private let columns: Int
+    private var list: [[T]] = []
+    private let content: (T) -> Content
+    
+    init(columns: Int, list: [T], @ViewBuilder content: @escaping (T) -> Content) {
+        self.columns = columns
+        self.content = content
+        self.setupList(list)
+    }
+    
+    private mutating func setupList(_ list: [T]) {
+        var column = 0
+        var columnIndex = 0
+        
+        for object in list {
+            if columnIndex < self.columns {
+                if columnIndex == 0 {
+                    self.list.insert([object], at: column)
+                    columnIndex += 1
+                } else {
+                    self.list[column].append(object)
+                    columnIndex += 1
+                }
+            } else {
+                column += 1
+                self.list.insert([object], at: column)
+                columnIndex = 1
+            }
+        }
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: .zero) {
+            ForEach(0 ..< self.list.count, id: \.self) { i  in
+                HStack(alignment: .top, spacing: .zero) {
+                    ForEach(self.list[i], id: \.self) { object in
+                            self.content(object)
+                    }
+                    if self.list[i].count == 1 {
+                        Spacer()
+                    }
+                }
+            }
+        }
+    }
+}

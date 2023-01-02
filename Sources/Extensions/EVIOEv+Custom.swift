f9ccd9736977dbc58efa21f5b7dcf91c07e653bf
Joao Moreira <joao.moreira@go-evio.com>
2023-01-02 14:56:58 +0000

    Fixed volkswagen name

--- Sources/Extensions/EVIOEv+Custom.swift
+++ Sources/Extensions/EVIOEv+Custom.swift
@@ -18,7 +18,7 @@ public extension EVIOEv {
     }
     static var id3: EVIOEv {
         let evInfo: EVIOEvInfo = EVIOEvInfo(id: nil, plugsChargingTable: nil, plugsFastChargingTable: nil, plugs: nil, databaseVehicleID: nil, yearFrom: nil, yearTo: nil, range: nil, useableBatteryCapacity: 58.0, maxBatteryCapacity: 62.0, eletricMotorPower: nil, internalChargerPower: 11.0, internalChargerChargeTime: 375, internalChargerChargeSpeed: 56, maxFastChargingPower: 124.0, avgFastChargingPower: nil, fastchargeChargeTime: 31, fastchargeChargeSpeed: 470, evType: nil, evEfficiency: nil, consumptionCity: nil, consumptionHighway: nil)
-        let ev: EVIOEv = EVIOEv(primaryEV: nil, status: nil, hasFleet: nil, usageNumber: nil, id: nil, evId: nil, otherInfo: nil, version: nil, model: "ID.3", imageContent: nil, licensePlate: nil, country: nil, brand: "Wolksvagen", fleet: nil, sessions: nil, listOfGroupDrivers: nil, listOfDrivers: nil, userID: nil, evType: nil, evInfo: evInfo, createdAt: nil, updatedAt: nil, v: nil, contractID: nil)
+        let ev: EVIOEv = EVIOEv(primaryEV: nil, status: nil, hasFleet: nil, usageNumber: nil, id: nil, evId: nil, otherInfo: nil, version: nil, model: "ID.3", imageContent: nil, licensePlate: nil, country: nil, brand: "Volkswagen", fleet: nil, sessions: nil, listOfGroupDrivers: nil, listOfDrivers: nil, userID: nil, evType: nil, evInfo: evInfo, createdAt: nil, updatedAt: nil, v: nil, contractID: nil)
         return ev
     }
     

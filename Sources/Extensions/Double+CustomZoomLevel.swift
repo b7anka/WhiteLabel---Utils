2c401ea8591086284186886e2607b70838409114
Joao Moreira <joao.moreira@go-evio.com>
2023-01-02 13:09:51 +0000

    Changed how decimal places are calculated

--- Sources/Extensions/Double+CustomZoomLevel.swift
+++ Sources/Extensions/Double+CustomZoomLevel.swift
@@ -14,7 +14,6 @@ public extension Double {
     static let defaultMapZoom: Double = 12.0
     
     func numberOfDecimalPlaces(maxPlaces: Int = 4) -> Int {
-        guard self > 0 else {return 0}
         let components: [String] = "\(self)".components(separatedBy: ".")
         guard components.count == 2, var decimalPlaces: String = components.last else {return 0}
         if decimalPlaces.count == 1, let decimalPlace = decimalPlaces.first {
